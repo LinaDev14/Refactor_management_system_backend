cc79cb68d918856b497393eb7b5b95ed0e5159e5
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 12:51:50 -0500

    Se completa Country

--- src/main/java/com/co/linadev/refactor_backend_mysql/domain/valueobject/Country.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/domain/valueobject/Country.java
@@ -1,3 +1,11 @@
 package com.co.linadev.refactor_backend_mysql.domain.valueobject;
 public enum Country {
+
+    COLOMBIA,
+    PERU,
+    PANAMA,
+    VENEZUELA,
+    BRAZIL,
+    BOLIVIA,
+    ECUADOR
 }
