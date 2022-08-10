351e82f09cbc44cb8e400e3b449e33558e009905
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 12:49:19 -0500

    Se completa employeeDTO

--- src/main/java/com/co/linadev/refactor_backend_mysql/domain/dto/EmployeeDTO.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/domain/dto/EmployeeDTO.java
@@ -1,4 +1,21 @@
 package com.co.linadev.refactor_backend_mysql.domain.dto;
 
+import com.co.linadev.refactor_backend_mysql.domain.valueobject.Country;
+import com.co.linadev.refactor_backend_mysql.domain.valueobject.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EmployeeDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Gender gender;
+    private Country country;
 }
