381d43588782a91b08deb8a0598730a0d954c752
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 15:16:20 -0500

    se modifica employee dto

--- src/main/java/com/co/linadev/refactor_backend_mysql/domain/dto/EmployeeDTO.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/domain/dto/EmployeeDTO.java
@@ -1,7 +1,6 @@
 package com.co.linadev.refactor_backend_mysql.domain.dto;
 
-import com.co.linadev.refactor_backend_mysql.domain.valueobject.Country;
-import com.co.linadev.refactor_backend_mysql.domain.valueobject.Gender;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +16,5 @@ public class EmployeeDTO {
     private String lastName;
     private String email;
     private String phoneNumber;
-    private Gender gender;
-    private Country country;
+
 }
