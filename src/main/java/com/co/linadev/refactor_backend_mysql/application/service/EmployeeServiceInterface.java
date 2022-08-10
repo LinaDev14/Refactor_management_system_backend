3bfd829f1a6ed1c8b6e6343b06a27bc862b72332
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 13:33:57 -0500

    se crea message y data.json en utils

--- src/main/java/com/co/linadev/refactor_backend_mysql/application/service/EmployeeServiceInterface.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/application/service/EmployeeServiceInterface.java
@@ -1,3 +1,27 @@
 package com.co.linadev.refactor_backend_mysql.application.service;
 public interface EmployeeServiceInterface {
+
+    //get employees
+
+    // get employees by id
+
+    // get employees by name
+
+    // get employees by first name
+
+    // get employees by country
+
+    // get employees by gender
+
+    // get employees by countries and gender
+
+    // update employee
+
+    // save a new employee
+
+    // save employees
+
+    // delete employee by id
+
+    // delete all employees
 }
---
+++ src/main/java/com/co/linadev/refactor_backend_mysql/domain/utils/Message.java
@@ -0,0 +1,3 @@
+package com.co.linadev.refactor_backend_mysql.domain.utils;
+public class Message {
+}
