afcce33efb67af29cc23d6c626abcbd4f1d57dea
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 11:45:04 -0500

    se inicia proyecto con spring

---
+++ src/main/java/com/co/linadev/refactor_backend_mysql/RefactorBackendMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.co.linadev.refactor_backend_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorBackendMysqlApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RefactorBackendMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/co/linadev/refactor_backend_mysql/RefactorBackendMysqlApplicationTests.java
@@ -0,0 +1,14 @@
+package com.co.linadev.refactor_backend_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorBackendMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
