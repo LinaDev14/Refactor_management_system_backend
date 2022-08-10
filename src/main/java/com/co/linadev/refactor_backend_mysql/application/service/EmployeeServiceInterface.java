4d8d3e34b3795a4c4c07e4da6b9e9328b48d6680
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 13:41:35 -0500

    se crea servicio listEmployee, findById, fillData

--- src/main/java/com/co/linadev/refactor_backend_mysql/application/service/EmployeeServiceInterface.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/application/service/EmployeeServiceInterface.java
@@ -1,25 +1,34 @@
 package com.co.linadev.refactor_backend_mysql.application.service;
+import com.co.linadev.refactor_backend_mysql.domain.dto.EmployeeDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface EmployeeServiceInterface {
 
     //get employees
+    List<EmployeeDTO> listEmployee();
 
     // get employees by id
+    Optional<EmployeeDTO> findById(Long id);
+    // find employees by name
 
-    // get employees by name
+    // find employees by first name
 
-    // get employees by first name
+    // find employees by country
 
-    // get employees by country
+    // find employees by gender
 
-    // get employees by gender
+    // find employees by name and country
 
-    // get employees by countries and gender
+    // find employees by countries and gender
 
     // update employee
 
     // save a new employee
 
     // save employees
+    List<EmployeeDTO> fillData(List<EmployeeDTO> employeeDTO);
 
     // delete employee by id
 
--- src/main/java/com/co/linadev/refactor_backend_mysql/domain/repository/EmployeeRepository.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/domain/repository/EmployeeRepository.java
@@ -4,4 +4,16 @@ import com.co.linadev.refactor_backend_mysql.domain.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    // find employees by name
+
+    // find employees by first name
+
+    // find employees by country
+
+    // find employees by gender
+
+    // find employees by name and country
+
+    // find employees by countries and gender
 }
