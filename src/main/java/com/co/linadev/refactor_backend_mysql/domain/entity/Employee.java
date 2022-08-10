1779dc70e441719a704cc1fd5b3ef4b5250fb8b7
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 12:19:07 -0500

    se crea valueObject

--- src/main/java/com/co/linadev/refactor_backend_mysql/domain/entity/Employee.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/domain/entity/Employee.java
@@ -1,2 +1,13 @@
-package com.co.linadev.refactor_backend_mysql.domain.entity;public class Employee {
+package com.co.linadev.refactor_backend_mysql.domain.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Employee {
+
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String phoneNumber;
 }
