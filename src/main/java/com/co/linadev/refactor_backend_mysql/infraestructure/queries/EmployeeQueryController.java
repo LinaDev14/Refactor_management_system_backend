08ff5e66ad1025973ae47ff1f5532a50cc591fb7
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 16:15:52 -0500

    se crea query findById

--- src/main/java/com/co/linadev/refactor_backend_mysql/infraestructure/queries/EmployeeQueryController.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/infraestructure/queries/EmployeeQueryController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/query")
@@ -31,5 +33,11 @@ public class EmployeeQueryController {
     @GetMapping("/listEmployees")
     public ResponseEntity<List<EmployeeDTO>> listEmployee(){
         return new ResponseEntity<>(employeeService.listEmployee(), HttpStatus.OK);
+    }
+
+    // find by id
+    @GetMapping("/findEmployeeById")
+    public ResponseEntity<Optional<EmployeeDTO>> findById(@RequestParam Long id){
+        return new ResponseEntity<>(employeeService.findById(id), HttpStatus.OK);
     }
 }
