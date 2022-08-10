37c0cf0dfdad35ac73a97adde8bfcdf204232359
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 12:36:50 -0500

    se completa entity employee

--- src/main/java/com/co/linadev/refactor_backend_mysql/domain/entity/Employee.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/domain/entity/Employee.java
@@ -1,13 +1,25 @@
 package com.co.linadev.refactor_backend_mysql.domain.entity;
 
+import com.co.linadev.refactor_backend_mysql.domain.valueobject.Country;
+import com.co.linadev.refactor_backend_mysql.domain.valueobject.Gender;
+import lombok.*;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
+@Data
 public class Employee {
 
-    private final Long id;
-    private final String firstName;
-    private final String lastName;
-    private final String email;
-    private final String phoneNumber;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Gender gender;
+    private Country country;
 }
