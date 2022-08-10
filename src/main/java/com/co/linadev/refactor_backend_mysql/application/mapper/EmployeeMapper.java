f9e24f191590c8fca331a2087d49461045e2232e
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-10 13:18:29 -0500

    se crea function mapToEmployeeDTO

--- src/main/java/com/co/linadev/refactor_backend_mysql/application/mapper/EmployeeMapper.java
+++ src/main/java/com/co/linadev/refactor_backend_mysql/application/mapper/EmployeeMapper.java
@@ -1,3 +1,24 @@
 package com.co.linadev.refactor_backend_mysql.application.mapper;
+import com.co.linadev.refactor_backend_mysql.domain.dto.EmployeeDTO;
+import com.co.linadev.refactor_backend_mysql.domain.entity.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
 public class EmployeeMapper {
+
+    public Function<Employee, EmployeeDTO> mapToEmployeeDTO(){
+
+        return employee -> new EmployeeDTO(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getPhoneNumber(),
+                employee.getGender(),
+                employee.getCountry()
+        );
+
+    }
 }
